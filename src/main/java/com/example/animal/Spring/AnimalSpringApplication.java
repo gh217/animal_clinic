c43fd7167e5655b9e47e74caab72aae569178a0d
gh217 <easy22.smile.22@gmail.com>
2022-10-29 21:16:07 +0200

    initial project add

---
+++ src/main/java/com/example/animal/Spring/AnimalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.animal.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/animal/Spring/AnimalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animal.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
